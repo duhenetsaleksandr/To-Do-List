e9772d1b0e2328c319bb693c8ea4a348360c4778
duhenetsaleksandr <duhenets@gmail.com>
2021-11-13 21:00:42 +0200

    change structure js files

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,3 @@
-const addTaskValue = document.getElementById('add-task-value');
-const addTaskBtn = document.getElementById('add-task-btn');
-const editTaskBtn = document.getElementById('edit-task-btn');
-const listTask = document.getElementById('list-task');
-let currentEditTask = null;
-
 addTaskBtn.addEventListener('click', addNewTask);
 editTaskBtn.addEventListener('click', editTask);
 addTaskValue.addEventListener('keydown', submit);
---
+++ scripts/constant-values.js
@@ -0,0 +1,5 @@
+const addTaskValue = document.getElementById('add-task-value');
+const addTaskBtn = document.getElementById('add-task-btn');
+const editTaskBtn = document.getElementById('edit-task-btn');
+const listTask = document.getElementById('list-task');
+let currentEditTask = null;
