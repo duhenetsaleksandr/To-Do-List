4f5adf5972269f57d60b00a2588775939a082af4
duhenetsaleksandr <duhenets@gmail.com>
2021-11-14 11:17:04 +0200

    added validation change

--- scripts/listeners.js
+++ scripts/listeners.js
@@ -1,24 +1,35 @@
 function addNewTask(event) {
-   event.preventDefault();
+   if (Object.prototype.toString.call(event).slice(8, -1) === 'PointerEvent') {
+      event.preventDefault();
+   }
 
    const text = addTaskValue.value.trim();
    addTaskValue.value = '';
    addTaskValue.focus();
 
-   if (text.length === 0) return;
+   if (text.length === 0) return false;
    listTask.append(createTaskElement(text));
    saveToLocaleStorage();
+   return true;
 }
 
 function editTask(event) {
-   event.preventDefault();
+   if (Object.prototype.toString.call(event).slice(8, -1) === 'PointerEvent') {
+      event.preventDefault();
+   }
+   if (Object.prototype.toString.call(currentEditTask).slice(8, -1) !== 'HTMLDivElement') return false;
+   if (!currentEditTask.classList.contains('list-task__item')) return false;
    addTaskBtn.classList.toggle('hide-btn');
    editTaskBtn.classList.toggle('hide-btn');
-   if (addTaskValue.value.trim() === '') return;
+   if (addTaskValue.value.trim() === '') {
+      currentEditTask = null;
+      return false;
+   }
    currentEditTask.querySelector('.list-task__item__check').innerText = addTaskValue.value;
    currentEditTask = null;
    addTaskValue.value = '';
    saveToLocaleStorage();
+   return true;
 }
 
 function submit(event) {
