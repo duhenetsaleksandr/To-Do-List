c0c48862b0c245d9930979ffa4fd9ea019c9e65a
duhenetsaleksandr <duhenets@gmail.com>
2021-11-28 13:15:29 +0200

    added tests for html

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,19 +1,94 @@
 import {html} from "../__mocks__/html/html";
 
+jest.mock('../src/scripts/Model/Model');
+
 describe('On HTML', function () {
-    beforeEach(() => {
+    beforeAll(() => {
         document.body.innerHTML = html;
+        require('../src/scripts/app');
     });
 
     it('there should be a ul tag "list-task" for todos', function () {
         expect(document.querySelectorAll('#list-task').length).toBe(1);
     });
 
-    it('should add new task', function () {
-        require('../src/scripts/app');
+    it('should render three todos in start', function () {
+        expect(document.querySelectorAll('.list-task__item').length).toBe(3);
+    });
 
-        document.querySelector('#add-task-value').value = 'test';
+    it('should add new task to end of list', function () {
+        const title = 'New task in list todos';
+        document.querySelector('#add-task-value').value = title;
         document.querySelector('#add-task-btn').click();
-        expect(document.querySelectorAll('.list-task__item').length).toBe(1);
+        const lastTask = document.querySelector('.list-task__item:last-child');
+        expect(lastTask.firstElementChild.textContent).toBe(title);
+    });
+
+    it('should checked first task in list of todos', function () {
+        const checked = document.querySelectorAll('.list-task__item')[0].classList.contains('checked');
+        document.querySelectorAll('.list-task__item')[0].click();
+        expect(document.querySelectorAll('.list-task__item')[0].classList.contains('checked')).toBe(!checked);
+    });
+
+    it('should delete last task in list of todos', function () {
+        const lastTask = document.querySelector('.list-task__item:last-child');
+        const lastTaskId = lastTask.dataset.id;
+        lastTask.querySelector('.list-task__item__delete').click();
+        expect(document.querySelector('.list-task__item:last-child').dataset.id).not.toBe(lastTaskId);
+    });
+
+    it('should not to add new task if input value is empty', function () {
+        const amountTasks = document.querySelectorAll('.list-task__item').length;
+        document.querySelector('#add-task-value').value = '';
+        document.querySelector('#add-task-btn').click();
+        expect(document.querySelectorAll('.list-task__item').length).toBe(amountTasks);
+    });
+
+    it('should not to add new task if input value with empty spaces', function () {
+        const amountTasks = document.querySelectorAll('.list-task__item').length;
+        document.querySelector('#add-task-value').value = '   ';
+        document.querySelector('#add-task-btn').click();
+        expect(document.querySelectorAll('.list-task__item').length).toBe(amountTasks);
+    });
+
+    it('should edit task title', function () {
+        const task = document.querySelectorAll('.list-task__item')[0];
+        const taskTitle = task.firstElementChild.textContent;
+        task.querySelector('.list-task__item__edit').click();
+        document.querySelector('#add-task-value').value = document.querySelector('#add-task-value').value + ' test';
+        document.querySelector('#edit-task-btn').click();
+        expect(document.querySelectorAll('.list-task__item')[0].firstElementChild.textContent).toBe(taskTitle + ' test');
+    });
+
+    it('should not edit task if input value is empty', function () {
+        const task = document.querySelectorAll('.list-task__item')[0];
+        const taskTitle = task.firstElementChild.textContent;
+        task.querySelector('.list-task__item__edit').click();
+        document.querySelector('#add-task-value').value = '';
+        document.querySelector('#edit-task-btn').click();
+        expect(document.querySelectorAll('.list-task__item')[0].firstElementChild.textContent).toBe(taskTitle);
+    });
+
+    it('should not edit task if input value with empty spaces', function () {
+        const task = document.querySelectorAll('.list-task__item')[0];
+        const taskTitle = task.firstElementChild.textContent;
+        task.querySelector('.list-task__item__edit').click();
+        document.querySelector('#add-task-value').value = '   ';
+        document.querySelector('#edit-task-btn').click();
+        expect(document.querySelectorAll('.list-task__item')[0].firstElementChild.textContent).toBe(taskTitle);
+    });
+
+    it('should editBtn is hide default', function () {
+        expect(document.querySelector('#edit-task-btn').classList.contains('hide-btn')).toBeTruthy();
+    });
+
+    it('should addTaskBtn is hide if task edit right now', function () {
+        document.querySelectorAll('.list-task__item .list-task__item__edit')[0].click();
+        expect(document.querySelector('#add-task-btn').classList.contains('hide-btn')).toBeTruthy();
+    });
+
+    it('should editBtn is not hide if task is edit right now', function () {
+        document.querySelectorAll('.list-task__item .list-task__item__edit')[0].click();
+        expect(document.querySelector('#edit-task-btn').classList.contains('hide-btn')).toBeFalsy();
     });
 });
---
+++ src/scripts/Model/__mocks__/Model.js
@@ -0,0 +1,98 @@
+export class ToDoModel {
+    constructor(view) {
+        this.view = view;
+        this.data = [];
+        this.fetchTodos = this.fetchTodos.bind(this);
+        this.addNewTask = this.addNewTask.bind(this);
+        this.checkedTask = this.checkedTask.bind(this);
+        this.deleteTask = this.deleteTask.bind(this);
+        this.editTaskBtn = this.editTaskBtn.bind(this);
+        this.fetchTodos();
+    }
+
+    fetchTodos() {
+        try {
+            this.view.loaderState('run');
+            this.data = [
+                {
+                    "userId": 1,
+                    "id": 1,
+                    "title": "delectus aut autem",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 2,
+                    "title": "quis ut nam facilis et officia qui",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 3,
+                    "title": "fugiat veniam minus",
+                    "completed": false
+                },
+            ];
+            this.view.renderList(this.data);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            this.view.loaderState('stop');
+        }
+    }
+
+    addNewTask(todo) {
+        try {
+            this.view.loaderState('run');
+            this.data.push({
+                "userId": 1,
+                "id": 201,
+                ...todo
+            });
+            this.view.renderList(this.data);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            this.view.loaderState('stop');
+        }
+    }
+
+    checkedTask(id) {
+        try {
+            this.view.loaderState('run');
+            let todo = this.data.find(todo => todo.id === id);
+            todo.completed = !todo.completed;
+            this.view.renderList(this.data);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            this.view.loaderState('stop');
+        }
+    }
+
+    deleteTask(id) {
+        try {
+            this.view.loaderState('run');
+            let index = this.data.findIndex(todo => todo.id === id);
+            this.data.splice(index, 1);
+            this.view.renderList(this.data);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            this.view.loaderState('stop');
+        }
+    }
+
+    editTaskBtn(id, title) {
+        try {
+            this.view.loaderState('run');
+            let todo = this.data.find(todo => todo.id === id);
+            todo.title = title;
+            this.view.renderList(this.data);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            this.view.loaderState('stop');
+        }
+    }
+}
